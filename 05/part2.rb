f96846ed665c1baaa92143c3953ea18c6e2c8833
Alexandre IGNJATOVIC <alexandre.ignjatovic@doctolib.com>
2020-12-06 07:06:34 +0100

    Day 5 shameless fix

--- 05/part2.rb
+++ 05/part2.rb
@@ -1,15 +1,5 @@
-ROWS = 7.times.reduce(['']) { |roots, index| r = []; %w(F B).each { |c| roots.each { |prefix| r << prefix + c } }; r }.sort.reverse
-
-def row(string)
-  ROWS.index(string[0..6])
-end
-
-def column(string)
-  %w[LLL LLR LRL LRR RLL RLR RRL RRR].index(string[-3..-1])
-end
-
 def seat_id(string)
-  8 * row(string) + column(string)
+  string.gsub(/[FL]/, '0').gsub(/[BR]/, '1').to_i(2)
 end
 
 seat_ids = File.readlines('input').map { |line| seat_id(line.chomp) }
