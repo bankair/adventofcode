dfbd4f5eb8a134cbacc9b9c4f8f2bc7e249cb810
Alexandre IGNJATOVIC <alexandre.ignjatovic@doctolib.com>
2020-12-15 12:43:44 +0100

    Day 15

---
+++ 15/part2.rb
@@ -0,0 +1,24 @@
+require 'byebug'
+
+ITERATIONS = 30000000
+
+def process(input)
+  numbers = input.split(/,/).map(&:to_i)
+  hash = Hash[numbers[0..-2].each_with_index.to_a.map { |a, b| [a, b + 1] }]
+  count = hash.size
+  number = numbers.last
+  loop do
+    return number if count == ITERATIONS - 1
+    count += 1
+    if hash[number]
+      new_number = count - hash[number]
+      hash[number] = count
+      number = new_number
+    else
+      hash[number] = count
+      number = 0
+    end
+  end
+end
+
+puts "Final result: #{process('0,1,4,13,15,12,16')}"
