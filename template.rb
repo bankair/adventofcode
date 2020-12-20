bee7129c46e43b2c7e65911a7139b39ad8d41bb2
Alexandre IGNJATOVIC <alexandre.ignjatovic@doctolib.com>
2020-12-20 09:42:34 +0100

    Edit template

--- template.rb
+++ template.rb
@@ -3,7 +3,17 @@ require 'byebug'
 def process(input)
 end
 
-puts process(<<TEST).inspect
+def test(input, expected)
+  actual = process(input)
+  if actual != expected
+    puts "ERROR: #{actual.inspect} != #{expected}"
+    puts "INPUT:\n#{input}"
+    exit 1
+  end
+  puts "SUCCESS => #{actual.inspect}"
+end
+
+test(<<TEST, 42).inspect
 My test input
 TEST
 
